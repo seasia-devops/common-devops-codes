912115890a22f75065b69b814e9168de18271265
Shivani Saini <52560286+shivani3495@users.noreply.github.com>
2024-07-29 12:52:33 +0530

    Teraform codes

---
+++ angular-eks/pipeline.tf
@@ -0,0 +1,122 @@
+resource "aws_codepipeline" "angular_pipeline" {
+  name     = "angular_pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+  tags     = {
+    Environment = var.env
+  }
+
+  artifact_store {
+    location = var.artifacts_bucket_name
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      input_artifacts  = []
+      output_artifacts = ["SourceArtifact"]
+
+      configuration = {
+        Owner      = var.github_owner
+        Repo       = var.github_repo
+        Branch     = var.github_branch
+        OAuthToken = var.github_token
+      }
+    }
+  }
+  stage {
+    name = "Build"
+
+    action {
+      category = "Build"
+      configuration = {
+        "EnvironmentVariables" = jsonencode(
+          [
+            {
+              name  = "environment"
+              type  = "PLAINTEXT"
+              value = var.env
+            },
+          ]
+        )
+        "ProjectName" = "angular_build"
+      }
+      input_artifacts = [
+        "SourceArtifact",
+      ]
+      name = "Build"
+      output_artifacts = [
+        "BuildArtifact",
+      ]
+      owner     = "AWS"
+      provider  = "CodeBuild"
+      run_order = 1
+      version   = "1"
+    }
+  }
+  
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "test-role-angular"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "codepipeline_policy_angular"
+  role = aws_iam_role.codepipeline_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+        "s3:GetBucketVersioning",
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "*" 
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:BatchGetBuilds",
+        "codebuild:StartBuild"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codedeploy:*"  
+      ],
+      "Resource": "*"
+    }
+  ]
+ }
+EOF
+}
